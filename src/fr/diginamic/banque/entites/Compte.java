05f02304acb601f85851b201152d82515530c055
florian <furnari.florian@hotmail.fr>
2023-04-12 14:53:41 +0200

    tp 5 fini

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,26 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+	
+
+private String numero;
+private int solde;
+
+public Compte(String numero, int solde) {
+	this.numero = numero;
+	this.solde = solde;
+}
+public String getNumero() {
+	return numero;
+}
+public void setNumero(String numero) {
+	this.numero = numero;
+}
+public double getSolde() {
+	return solde;
+}
+public void setSolde(int solde) {
+	this.solde = solde;
+}
+}
+
---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,9 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+	public static void main(String[] args) {
+		
+		Compte cpt = new Compte("a1245", 755500);
+		System.out.println(cpt);
+	}
+}
