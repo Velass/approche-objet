840d04023b9f2be2004517ffa4afab87a833a86b
florian <furnari.florian@hotmail.fr>
2023-04-12 12:32:25 +0200

    tp 4 fini

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class TestMethodeStatic {
+ public static void main(String[] args) {
+	 String chaine = "12";
+	 System.out.println(chaine);
+	 
+	int valeur = Integer.parseInt(chaine);
+
+	 System.out.println(valeur);
+	 
+	 int a = 124;
+	 int b = 82;
+	
+	 System.out.println(Integer.max(a, b));
+
+}
+}
